6b2748f5239c2a148c734f07545cc9a1ea4f9379
noahburrows <w0772238@myscc.ca>
2023-06-09 14:02:31 -0400

    Add support email intent

--- app/src/main/java/com/example/taskman/Fragments/CreditsFragment.java
+++ app/src/main/java/com/example/taskman/Fragments/CreditsFragment.java
@@ -1,5 +1,8 @@
 package com.example.taskman.Fragments;
 
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -11,50 +14,19 @@ import android.widget.TextView;
 
 import com.example.taskman.R;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link CreditsFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+
 public class CreditsFragment extends Fragment {
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
 
     public CreditsFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment CreditsFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static CreditsFragment newInstance(String param1, String param2) {
-        CreditsFragment fragment = new CreditsFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
+
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
         }
     }
 
@@ -67,6 +39,20 @@ public class CreditsFragment extends Fragment {
         TextView imageLink = view.findViewById(R.id.provider);
         TextView apiLink = view.findViewById(R.id.api);
         TextView supportLink = view.findViewById(R.id.support);
+
+
+        supportLink.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Intent.ACTION_SENDTO);
+                intent.setData(Uri.parse("mailto:")); // Only email apps handle this.
+                intent.putExtra(Intent.EXTRA_EMAIL, new String[]{"w0772238@myscc.ca"});
+                intent.putExtra(Intent.EXTRA_SUBJECT, "TaskMan Support");
+                if (intent.resolveActivity(getContext().getPackageManager()) != null) {
+                    startActivity(intent);
+                }
+            }
+        });
 
 
         return view;
